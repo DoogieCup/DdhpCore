8bbc21a707cf140cc5f9bd6dd6eb74722e1c5ec8
Simon Duff <simon.j.duff@gmail.com>
2016-11-15 20:09:31 +0000

    Introduced a WebJob worker app

---
+++ src/Workers/DdhpCore.Workers.DefaultWorker/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DdhpCore.Workers.DefaultWorker
+{
+    /// <summary>
+    /// Any queue handlers here must be agnostic as to whether they are running on
+    /// Azure ServiceBus, Azure Queue Storage, or AWS SNS/SQS.
+    /// We should be able to switch between those implementations with ease.
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ src/Workers/DdhpCore.Workers.DefaultWorker/run.cmd
@@ -0,0 +1,2 @@
+﻿@echo off
+dotnet DotNetCoreWebJobSample.ConsoleApp.dll
